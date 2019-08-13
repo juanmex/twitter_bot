4df20b0e0f312be74c5b4b4426011353efb72285
Alejandro Cen <alejandrocen@techsystems.com.mx>
2019-08-12 21:00:38 -0500

    get timeline by screen name expect test

--- spec/twitter_bot_spec.rb
+++ spec/twitter_bot_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe TwitterBot do
 
   subject(:bot) { 
     # TwitterBot::Bot.new({
-    #                     :consumer_key => "IsiZffYDlPUq3Y6EiPLqMGyNL",
-    #                     :consumer_secret => "rfxDqAiU1VQDdewevu1u4HZO7X1O1KeKUhaiTZYUx04NpMjV6a",
-    #                     :token => "112822368-iPMcKaVnl34o8I6SB5y8hwsHteR6wZeuCyABFhqr",
-    #                     :token_secret => "3kfFyxWoqk819NZx0hy9fF796p7ZxxsxK4wgTESgsN3Sr"
-    #                     })
+    #   :consumer_key => "IsiZffYDlPUq3Y6EiPLqMGyNL",
+    #   :consumer_secret => "rfxDqAiU1VQDdewevu1u4HZO7X1O1KeKUhaiTZYUx04NpMjV6a",
+    #   :token => "112822368-iPMcKaVnl34o8I6SB5y8hwsHteR6wZeuCyABFhqr",
+    #   :token_secret => "3kfFyxWoqk819NZx0hy9fF796p7ZxxsxK4wgTESgsN3Sr"
+    # })
     
     url = URI("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=freddier&count=2")
 
@@ -21,10 +21,11 @@ RSpec.describe TwitterBot do
 
     response = http.request(request)
     puts response.read_body
+    "true"
   }
 
-  it "send a tweet" do  
-    expect(bot.tweet).to eq("true")
+  it "get timeline by screen name" do  
+    expect(bot).to eq("true")
   end
 
 end
